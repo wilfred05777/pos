2da33d2dcac356002d04a79aa6dce0a0360ff311
Wilfred <wilfred05777@gmail.com>
2023-05-13 04:50:06 +0800

    122. How to Display Login Credentials Message

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -62,15 +62,25 @@
 
                                 <div class="mb-3">
                                     <label for="login" class="form-label">Email/Name/Phone</label>
-                                    <input class="form-control" type="text" id="login" name="login"
-                                        required="" placeholder="Enter your Name/Email/Phone">
+                                    <input class="form-control @error('login') is-invalid @enderror" type="text"
+                                        id="login" name="login" required=""
+                                        placeholder="Enter your Name/Email/Phone">
+                                    @error('login')
+                                        <span class="text-danger">
+                                            {{ $message }}
+                                        </span>
+                                    @enderror
                                 </div>
 
                                 <div class="mb-3">
                                     <label for="password" class="form-label">Password</label>
                                     <div class="input-group input-group-merge">
-                                        <input type="password" id="password" name="password" class="form-control"
+                                        <input type="password" id="password" name="password"
+                                            class="form-control @error('password') is-invalid @enderror"
                                             placeholder="Enter your password">
+                                        @error('password')
+                                            <span class="text-danger">{{ $message }}</span>
+                                        @enderror
                                         <div class="input-group-text" data-password="false">
                                             <span class="password-eye"></span>
                                         </div>
