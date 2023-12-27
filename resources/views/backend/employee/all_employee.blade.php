3a340870c0ddfa110cc1eea5e05d3fa16100ac82
Wilfred Erdo <wilfred05777@users.noreply.github.com>
2023-12-27 20:20:37 +0800

    129. Employee Management Part 3 - v2

--- resources/views/backend/employee/all_employee.blade.php
+++ resources/views/backend/employee/all_employee.blade.php
@@ -11,9 +11,11 @@
                     <div class="page-title-box">
                         <div class="page-title-right">
                             <ol class="breadcrumb m-0">
-                                <li class="breadcrumb-item"><a href="javascript: void(0);">UBold</a></li>
+                                <a a href="" class="btn btn-primary rounded-pill waves-effect waves-light ">Add
+                                    Employee</a>
+                                {{-- <li class="breadcrumb-item"><a href="javascript: void(0);">UBold</a></li>
                                 <li class="breadcrumb-item"><a href="javascript: void(0);">Tables</a></li>
-                                <li class="breadcrumb-item active">Datatables</li>
+                                <li class="breadcrumb-item active">Datatables</li> --}}
                             </ol>
                         </div>
                         <h4 class="page-title">Datatables</h4>
@@ -54,9 +56,9 @@
                                             <td>{{ $item->salary }}</td>
                                             <td>
                                                 <a a href=""
-                                                    class="btn btn-blue rounded-pill waves-effect waves-light "></a>
+                                                    class="btn btn-blue rounded-pill waves-effect waves-light ">Edit</a>
                                                 <a a href=""
-                                                    class="btn btn-danger rounded-pill waves-effect waves-light"></a>
+                                                    class="btn btn-danger rounded-pill waves-effect waves-light">Delete</a>
                                             </td>
                                         </tr>
                                     @endforeach
